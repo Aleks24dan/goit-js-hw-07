cddd028d7ad05130709acee5ae159e8a79c3c507
Aleks24dan <aleks24dan@gmail.com>
2021-06-02 23:02:47 +0200

    fixed one more bug

--- js/task-01.js
+++ js/task-01.js
@@ -4,14 +4,14 @@
 // console.log(`Количество элементов:${itemEl.length}`)
 
 const categoriesItem = document.querySelectorAll('.item')
-const arrCategories = [...categoriesItem];
 let count = 0;
-arrCategories.forEach((el, i) => {
+categoriesItem.forEach((el, i) => {
     count += (i); 
 });
+
 console.log(`В списке ${count} категории`);
 
-arrCategories.forEach((el) => {
+categoriesItem.forEach((el) => {
    const textTitle = el.querySelector('h2')
     console.log(`Категория:${textTitle.textContent}`)
     const elementOfCategory = el.querySelectorAll('ul li')
